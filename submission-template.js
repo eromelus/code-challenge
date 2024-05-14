01bc32d4e82411bfc4ffa70c6fc46262ef2b22bc
Eddie <edwinsromelus@gmail.com>
2024-05-14 15:25:39 -0500

    add answers to submission.js

--- submission-template.js
+++ submission-template.js
@@ -1,23 +1,96 @@
-const findSum = function(array) {
-  // your code here - don't forget to return a number!
+const findSum = function (array) {
+  let sum = 0;
+  array.forEach((number) => (sum += number));
+  return sum;
 };
 
-const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+const findFrequency = function (array) {
+  const itemFrequencyCount = {};
+
+  const result = {
+    most: array[0],
+    least: array[0],
+  };
+
+  for (let item in itemFrequencyCount) {
+    if (itemFrequencyCount[item] > itemFrequencyCount[result.most]) {
+      result.most = item;
+    }
+
+    if (itemFrequencyCount[item] < itemFrequencyCount) {
+      result.least = item;
+    }
+  }
+
+  return result;
 };
 
-const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+const isPalindrome = function (str) {
+  const lowerCasedString = str.toLowerCase();
+  const reversedString = lowerCasedString.split("").reverse().join("");
+
+  return lowerCasedString === reversedString;
 };
 
-const largestPair = function(array) {
-  // your code here - don't forget to return a number!
+const largestPair = function (array) {
+  let maxProduct = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    const product = array[i] * array[i + 1];
+
+    if (product > maxProduct) {
+      maxProduct = product;
+    }
+  }
+
+  return maxProduct;
 };
 
-const removeParenth = function(str) {
-  // your code here - don't forget to return a string!
+const removeParenth = function (str) {
+  const openParenthIndex = str.indexOf("(");
+  const closedParenthIndex = str.indexOf(")");
+
+  return str.slice(0, openParenthIndex) + str.slice(closedParenthIndex + 1);
 };
 
-const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+const scoreScrabble = function (str) {
+  const letterScores = {
+    a: 1,
+    e: 1,
+    i: 1,
+    o: 1,
+    u: 1,
+    l: 1,
+    i: 1,
+    n: 1,
+    r: 1,
+    s: 1,
+    t: 1,
+    d: 2,
+    g: 2,
+    b: 3,
+    c: 3,
+    m: 2,
+    p: 3,
+    f: 4,
+    h: 4,
+    v: 4,
+    w: 4,
+    y: 4,
+    k: 5,
+    j: 8,
+    x: 8,
+    q: 10,
+    z: 10,
+  };
+
+  let score = 0;
+  str
+    .toLowerCase()
+    .split("")
+    .forEach((char) => {
+      score += letterScores[char];
+    });
+
+  return score;
 };
